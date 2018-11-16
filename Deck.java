b5604645278f0e0ab4e957f9ebb4eaff4b1ddc2f
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-16 12:00:13 -0800

    push push

--- Deck.java
+++ Deck.java
@@ -1,33 +1,10 @@
-
-/**
- * Write a description of class Deck here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Deck
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Deck
-     */
-    public Deck()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+import java.util.*;
+public class Deck{
+    public Deck(){
+        ArrayList<Card> myDeck = new ArrayList<Card>();
+        for (int i = 0; i < 14; i++){
+            Card myCard = new Card(i, i);
+            myDeck.add(myCard);
+        }
     }
 }
