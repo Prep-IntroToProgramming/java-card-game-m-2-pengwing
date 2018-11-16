febed2dec1bccd1fe926e1e0d4a5c7c9141b01c9
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-16 11:48:12 -0800

    Initial sharing of project

---
+++ CardTestDrive.java
@@ -0,0 +1,12 @@
+class CardTestDrive {
+    public static void main(String[] args){
+        int testRank = Card.THREE;
+        int testSuit = Card.DIAMONDS;
+        Card card1 = new Card(testSuit, testRank);
+        if ((card1.rank == testRank) && (card1.suit == testSuit)){
+            System.out.println(card1.rankToString(testRank) + " of " + card1.suitToString(testSuit));
+        } else {
+            System.out.println("no");
+        }
+    }  
+}
