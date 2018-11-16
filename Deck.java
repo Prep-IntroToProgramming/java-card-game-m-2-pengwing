8863c9a500dfd5e60ec2d3cf472941c839fc26d0
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-16 12:08:12 -0800

    deck constructor

--- Deck.java
+++ Deck.java
@@ -2,9 +2,11 @@ import java.util.*;
 public class Deck{
     public Deck(){
         ArrayList<Card> myDeck = new ArrayList<Card>();
-        for (int i = 0; i < 14; i++){
-            Card myCard = new Card(i, i);
-            myDeck.add(myCard);
+        for (int suit = 1; suit < 5; suit++){
+            for (int rank = 1; suit < 14; rank++){
+                Card myCard = new Card(suit, rank);
+                myDeck.add(myCard);
+            }
         }
     }
 }
