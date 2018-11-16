631abbeafec3c52b201f46920ffa65ead502bdfc
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-16 12:20:23 -0800

    testing deck constructor

--- Deck.java
+++ Deck.java
@@ -1,11 +1,11 @@
 import java.util.*;
 public class Deck{
+    ArrayList<Card> cards = new ArrayList<Card>();
     public Deck(){
-        ArrayList<Card> myDeck = new ArrayList<Card>();
         for (int suit = 1; suit < 5; suit++){
             for (int rank = 1; suit < 14; rank++){
                 Card myCard = new Card(suit, rank);
-                myDeck.add(myCard);
+                cards.add(myCard);
             }
         }
     }
---
+++ DeckTestDrive.java
@@ -0,0 +1,7 @@
+public class DeckTestDrive {
+    public static void main(String[] args){
+        Deck newDeck = new Deck();
+        System.out.println(newDeck.cards.size());
+        
+    }
+}
