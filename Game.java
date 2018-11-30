b9791b1b7836759f96e8ccb1180faed4df5e82f3
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-30 12:18:45 -0800

    adding to game class

--- Game.java
+++ Game.java
@@ -8,20 +8,31 @@ public class Game {
         deck1.shuffle();
         for (int i = 0; i < 2; i++){
             Card c = deck1.draw();
-            p1.addCard(c);
+            p1.addCard(c); //adds two cards to player's hand
             Card x = deck1.draw();
-            dealer.addCard(x);
+            dealer.addCard(x); // adds two cards to dealer's hand
         }
         System.out.println("Your hand is:");
-        p1.showHand();
+        p1.showHand(); //runs show hand method for player's hand
+        System.out.println(" ");
         System.out.println("The dealer's hand is:");
-        dealer.showHand();
+        dealer.showHand(); //runs show hand method for dealer's hand
         if (p1.calcHand() == 21){
             System.out.println("you won!");
         }
         while (dealer.calcHand() < 17){
             Card c = deck1.draw();
             dealer.addCard(c);
+            if (dealer.calcHand() > 21){
+                dealer.showHand();
+                System.out.println("You win by default.. you're not special");
+            } else if (dealer.calcHand() == 21){
+                dealer.showHand();
+                System.out.println("you lose, SUCKER");
+            } else {
+                System.out.println("The dealer's new hand is:");
+                dealer.showHand();
+            }
         }
     }
 }
