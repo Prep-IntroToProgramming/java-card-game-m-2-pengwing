8aaeac5a8d084547618d2efddd5a893b1378bdb3
Mei-Li Okumura <19OkumuraM@flintridgeprep.org>
2018-11-29 20:47:11 -0800

    adding more to game class

--- Game.java
+++ Game.java
@@ -9,6 +9,19 @@ public class Game {
         for (int i = 0; i < 2; i++){
             Card c = deck1.draw();
             p1.addCard(c);
+            Card x = deck1.draw();
+            dealer.addCard(x);
+        }
+        System.out.println("The dealer's hand is:");
+        dealer.showHand();
+        System.out.println("Your hand is:");
+        p1.showHand();
+        if (p1.calcHand() == 21){
+            System.out.println("you won!");
+        }
+        while (dealer.calcHand() < 17){
+            Card c = deck1.draw();
+            dealer.addCard(c);
         }
     }
 }
