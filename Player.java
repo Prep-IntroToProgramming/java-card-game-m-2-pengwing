4ac8ae7b9ffa67aeac7d3a96bf729142a1ef32ed
Mei-Li Okumura <19OkumuraM@flintridgeprep.org>
2018-12-05 16:55:16 -0800

    player comments done

--- Player.java
+++ Player.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Player {
     //instance variables
-    ArrayList<Card> hand = new ArrayList<Card>();
+    ArrayList<Card> hand = new ArrayList<Card>(); //creates a new arraylist of cards called hand
     String name;
     Scanner input = new Scanner(System.in);
     /* lets user choose name
@@ -14,32 +14,48 @@ public class Player {
         input.nextLine();
     }
 
+    /*
+     * prints out the players hand
+     *      prints cards in the hand on a separate line 
+     *      using an enhanced for loop
+     */
     void showHand(){
         for (Card x: hand){
             System.out.println(x.rankToString() + " of " + x.suitToString());
         }
     }
 
+    /*
+     * takes a card c and adds it to the players hand
+     */
     void addCard(Card c){
         hand.add(c);
     }
 
+    /* 
+     * calculates the value of the players hand
+     * it adds each card to int handVal according to the value of the rank 
+     * NOTE: all face cards have value 10, so for all ranks that correspond to face cards, 
+     * program just adds 10, instead of rank value
+     * NOTE 2: acePresent - IF PLAYER HAS ACE, it allows the program to make sure that 
+     * the player has the highest possible value without exceeding 21
+     */
     int calcHand(){
         int handVal = 0;
         boolean acePresent = false;
         for (int i = 0; i < hand.size(); i++){
             if (hand.get(i).rank <= 10){
                 handVal = hand.get(i).rank + handVal;
-            } else {
+            } else { //see NOTE in above comment
                 handVal = handVal + 10;
             }
             if (hand.get(i).rank == 1){
                 acePresent = true;
             }
         }
-        if ((handVal <= 11) && (acePresent == true)){
+        if ((handVal <= 11) && (acePresent == true)){ //see NOTE 2 in above comment
             handVal = 10 + handVal;
         }
-        return handVal;
+        return handVal; //returns variable handVal to be printed/compared etc.
     }
 }
--- PlayerTestDrive.java
+++ PlayerTestDrive.java
@@ -1,5 +1,7 @@
 /* a test class to make sure the player methods
  * EDIT: THEY WORK!!!
+ * NOTE: ensures calcHand method treats the aces appropriately 
+ * based on other cards in hand
  */
 public class PlayerTestDrive {
     public static void main(String[] args){
@@ -9,14 +11,19 @@ public class PlayerTestDrive {
         p.addCard(new Card(4,1)); //creates an ACE OF SPADES for player 
         p.addCard(new Card(3,13)); //creates a KING OF HEARTS for player
         p.showHand(); 
-        System.out.println(p.calcHand()); // should be 12
+        System.out.println(p.calcHand()); // calcHand treats BOTH aces as 1; should be 12
 
         Player p1 = new Player();
         p1.addCard(new Card(3,10));
         p1.addCard(new Card(1,1));
         p1.showHand(); 
-        System.out.println(p1.calcHand()); // should be 21
+        System.out.println(p1.calcHand()); // calcHand treats ace as 1; should be 21
         
         Player p2 = new Player();
+        p2.addCard(new Card(3,10));
+        p2.addCard(new Card(1,1));
+        p2.addCard(new Card(2,4));
+        p2.showHand(); 
+        System.out.println(p2.calcHand()); // calcHand treats ace as 1 to not exceed 21; should be 15
     }
 }
