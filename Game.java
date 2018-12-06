b9d71168a1142afdbbb54a6e369a15386ca8c5d8
Mei-Li Okumura <19OkumuraM@flintridgeprep.org>
2018-12-05 17:22:36 -0800

    still trying to check user input

--- Game.java
+++ Game.java
@@ -34,9 +34,12 @@ public class Game {
         System.out.println(" ");
         while (true){ //true is always true so it continues until it breaks
             System.out.println("to hit, press 1. to hold, press 2"); //prompts user to hit or hold
-            int choice = input.nextInt();
-            while (!input.hasNextInt() || ((choice != 1) && (choice !=2))){
-                if (!input.hasNextInt()){
+            int choice = 0;
+            while (!input.hasNextInt() && (choice != 1) && (choice !=2)){
+                System.out.println("i said either 1 or 2 doofus");
+                input.nextLine();
+                choice = input.nextInt();
+                /*if (!input.hasNextInt()){
                     System.out.println("that's not an integer. try again");
                     input.nextLine();
                     //makes sure user CANNOT input anything other than an integer
@@ -47,15 +50,16 @@ public class Game {
                     choice = input.nextInt();
                     //makes sure user CANNOT input anything other than 1 or 2
                 }
+                */
             }
             /*
             while ((choice != 1) && (choice !=2)){
-                System.out.println("i said either 1 or 2 doofus");
-                input.nextLine();
-                choice = input.nextInt();
-                //makes sure user CANNOT input anything other than 1 or 2
+            System.out.println("i said either 1 or 2 doofus");
+            input.nextLine();
+            choice = input.nextInt();
+            //makes sure user CANNOT input anything other than 1 or 2
             }
-            */
+             */
 
             if (choice == 1){
                 Card c = deck1.draw(); //taking  card from deck
